9af1886efb7cbb91c339d989492197531127befe
Nikita Dymont <gg.joke55@gmail.com>
2023-07-01 18:31:10 +0300

    implement structure, configure redux toolkit store, create hooks

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
+import { setupStore } from "./store/store";
 import { App } from "./App.tsx";
 
 import "./index.css";
 
+const store = setupStore();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
